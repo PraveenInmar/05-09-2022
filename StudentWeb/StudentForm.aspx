81a04b5723114214ee9064be7fb6eaaa6bd68e98
PraveenInmar <111868809+PraveenInmar@users.noreply.github.com>
2022-09-05 15:53:44 +0530

    Add files via upload

---
+++ StudentWeb/StudentForm.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentForm.aspx.cs" Inherits="StudentWeb.StudentForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Roll No"></asp:Label>
+        <asp:TextBox ID="SRNINPUT" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="SNINPUT" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Age"></asp:Label>
+            <asp:TextBox ID="SAINPUT" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label4" runat="server" Text="Branch"></asp:Label>
+            <asp:TextBox ID="SBINPUT" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label5" runat="server" Text="College"></asp:Label>
+            <asp:TextBox ID="SCINPUT" runat="server"></asp:TextBox>
+        </p>
+        <asp:RadioButton ID="Male" runat="server" GroupName="GenderButton" Text="Male" />
+        <asp:RadioButton ID="Female" runat="server" GroupName="GenderButton" Text="Female" />
+        <p>
+            <asp:RadioButton ID="MarriedButton" runat="server" GroupName="Status" Text="Married" />
+            <asp:RadioButton ID="UnMarriedButton" runat="server" GroupName="Status" OnCheckedChanged="RadioButton4_CheckedChanged" Text="UnMarried" />
+        </p>
+        <asp:Button ID="ButtonAdd" runat="server" OnClick="ButtonAdd_Click" Text="Add" />
+        <asp:Button ID="ButtonUpdate" runat="server" OnClick="ButtonUpdate_Click" Text="Update" />
+        <asp:Button ID="ButtonDelete" runat="server" OnClick="ButtonDelete_Click" Text="Delete" />
+        <br />
+        <asp:GridView ID="GridViewStudent" runat="server" AutoGenerateSelectButton="True" OnSelectedIndexChanged="GridViewStudent_SelectedIndexChanged">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ StudentWeb/StudentForm.aspx.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataBaseConnection;
+using Host;
+using System.Data;
+
+
+namespace StudentWeb
+{
+    public partial class StudentForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadStudent();
+        }
+
+        protected void RadioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadStudent()
+        {
+            Code c = new Code();
+            GridViewStudent.DataSource = c.display();
+            GridViewStudent.DataBind();
+        }
+
+        protected void ButtonAdd_Click(object sender, EventArgs e)
+        {
+          
+            Hub hubobj = new Hub();
+            hubobj.RollNo1 = Convert.ToInt32(SRNINPUT.Text);
+            hubobj.Name1 = SNINPUT.Text;
+            hubobj.Age1 = Convert.ToInt32(SAINPUT.Text);
+            hubobj.Branch1 = SBINPUT.Text;
+            hubobj.College1 = SCINPUT.Text;
+
+            string gender = "";
+            string status = "";
+
+            if (Male.Checked)
+            {
+                gender = "Male";
+            }
+            else
+            {
+                gender = "Female";
+            }
+            hubobj.Gender1 = gender;
+
+            if (MarriedButton.Checked)
+            {
+                status = "Married";
+            }
+            else
+            {
+                status = "Unmarried";
+            }
+            hubobj.Status1 = status;
+
+            hubobj.adding();
+            LoadStudent();       
+        }
+        private void clearData()
+        {
+            SRNINPUT.Text = "";
+            SNINPUT.Text = "";
+            SAINPUT.Text = "";
+            SBINPUT.Text = "";
+            SCINPUT.Text = "";
+            Male.Checked = false;
+            Female.Checked = false;
+            MarriedButton.Checked = false;
+            UnMarriedButton.Checked = false;
+
+        }
+        //Select method and display
+        protected void GridViewStudent_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            clearData();
+            int name = Convert.ToInt32( GridViewStudent.Rows[GridViewStudent.SelectedIndex].Cells[1].Text);
+            DisplayScreen(name);
+        }
+        public void DisplayScreen(int student)
+        {
+            Hub hubobj = new Hub();
+            DataSet datasetobj = hubobj.searchDetails(student);
+
+            //int RollNo    = Convert.ToInt32( datasetobj.Tables[0].Rows[0][0].ToString());
+            string Name = datasetobj.Tables[0].Rows[0][1].ToString();
+            int Age = Convert.ToInt32(datasetobj.Tables[0].Rows[0][2].ToString());
+            string Branch = datasetobj.Tables[0].Rows[0][3].ToString();
+            string College = datasetobj.Tables[0].Rows[0][4].ToString();
+            string Gender = datasetobj.Tables[0].Rows[0][5].ToString();
+            string Status = datasetobj.Tables[0].Rows[0][6].ToString();
+
+            SRNINPUT.Text = student.ToString();
+            SNINPUT.Text = Name;
+            SAINPUT.Text = Age.ToString();
+            SBINPUT.Text = Branch;
+            SCINPUT.Text = College;
+
+            if (Gender == "Male")
+            {
+                Male.Checked = true;
+            }
+            else
+            {
+                Female.Checked = true;
+            }
+
+            if (Status == "Married")
+            {
+                MarriedButton.Checked = true;
+            }
+            else
+            {
+                UnMarriedButton.Checked = true;
+            }
+            //StatusgroupBox.Text = Status;
+        }
+
+        protected void ButtonUpdate_Click(object sender, EventArgs e)
+        {
+            Hub ho = new Hub();
+
+            int RollNo = Convert.ToInt32(SRNINPUT.Text);
+            string Name = SNINPUT.Text;
+            int age = Convert.ToInt32(SAINPUT.Text);
+            string branch = SBINPUT.Text;
+            string Collage = SCINPUT.Text;
+            string gender = "";
+            string status = "";
+            if (Male.Checked)
+            {
+                gender = "Male";
+            }
+            else
+            {
+                gender = "Female";
+            }
+            // hubobj.Gender1 = GenderGroupbox.Text;
+
+            if (MarriedButton.Checked)
+            {
+                status = "Married";
+            }
+            else
+            {
+                status = "Unmarried";
+            }
+            // hubobj.Status1 = StatusgroupBox.Text;
+            ho.Name1 = Name;
+            ho.Age1 = age;
+            ho.Branch1 = branch;
+            ho.College1 = Collage;
+            ho.Gender1 = gender;
+            ho.Status1 = status;
+            ho.RollNo1 = RollNo;
+            ho.updating();
+            LoadStudent();
+            clearData();
+        }
+
+        protected void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            Hub h = new Hub();
+            h.Name1 = SNINPUT.Text;
+            h.deleting();
+            clearData();
+            LoadStudent();
+
+        }
+    }
+}
